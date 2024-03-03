e28285b3b0a3522e5bba1b0a4b215ace39faa093
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-03-03 22:52:06 +0500

    Foundation of java

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,26 @@
+
+public class MyFirstProgram { // class class and methods name are called "identifiers"
+
+	// Classes consist of members such as : methods and property
+	// Access Modifiers : public and private are access modifiers, they give permission to access that class or method
+	
+	public static void main(String[] args) { // Methods: do something printing to console
+		System.out.println("Hello There!"); // statement
+
+	}
+	
+	public String x = new String("hello");  // Property : Store Values
+	
+}
+
+// comment
+// class - Contains everything - and has members
+// methods - do something 
+// arguments - What you pass to a method to be print (part of the calling)
+// parameters - Variables to store arguments (part of definition)
+// statements -  Telling the computer something
+// properties - store something
+
+// access modifier (public) - Who can use ? 
+// static - No instance of class is needed
+// object - instance of class
